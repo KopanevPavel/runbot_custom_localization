15d4503a59bc77ba7135a092be09de8ba784429b
Pavel <kopanev-pavel-spb@yandex.ru>
2021-03-21 12:23:09 +0300

    Removed ros build folders

--- frontend/SuperGlue_ROS/devel/share/SuperGlue_ROS/cmake/SuperGlue_ROSConfig.cmake
+++
@@ -1,1 +0,0 @@
-/home/jetson/SLAM/runbot_custom_localization/frontend/SuperGlue_ROS/devel/.private/SuperGlue_ROS/share/SuperGlue_ROS/cmake/SuperGlue_ROSConfig.cmake
--- frontend/SuperGlue_ROS/devel/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig-version.cmake
+++
@@ -1,1 +0,0 @@
-/home/jetson/SLAM/runbot_custom_localization/frontend/SuperGlue_ROS/devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig-version.cmake
